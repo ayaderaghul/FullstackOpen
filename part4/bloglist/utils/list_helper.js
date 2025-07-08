c585efdfab7c097ad2aecd5cf1f3e592fc55af80
Aya de Raghũl <nguyenlinhchi09@gmail.com>
2025-07-08 21:11:10 +0200

    -> ex 4.5

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,20 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => { 
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => { 
+    if (blogs.length === 0) return null
+    
+    return blogs.reduce((favorite, current) =>
+        current.likes > favorite.likes ? current : favorite)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
