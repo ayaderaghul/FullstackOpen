fe76269caa13396269a8df53835b9f93850f7aa3
Aya de Raghũl <nguyenlinhchi09@gmail.com>
2025-07-21 13:19:11 +0200

    ex 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,21 +3,27 @@ import { useMemo } from 'react' // Add useMemo hook
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => { 
+    const dispatch = useDispatch()
     // 1. Get raw anecdotes from state
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+            console.log(state)
+
+        if (state.filter === '') {
+            return state.anecdotes
+        }
+        return state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+    })
     
     // 2. Memoize sorted anecdotes
     const sortedAnecdotes = useMemo(() => {
         return [...anecdotes].sort((a, b) => b.votes - a.votes)
     }, [anecdotes]) // Only recompute when anecdotes change
 
-    const dispatch = useDispatch()
-
     const handleVote = (id) => { 
         dispatch(vote(id))
     }
 
-    return ( // Don't forget return statement!
+    return ( 
         <>
         {sortedAnecdotes.map(anecdote => (
             <div key={anecdote.id}>
